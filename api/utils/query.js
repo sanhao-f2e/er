3c41ce0b3bf4238c242196c24942e31715ce2a82
dingxueshu <dingxueshu@sanhao.com>
2019-04-11 08:56:52 +0800

    项目初始搭建

---
+++ api/utils/query.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql')
+const dbConf = require('../config/db')
+const pool = mysql.createPool(dbConf)
+
+let query = (sql, values) => {
+  return new Promise((resolve, reject) => {
+    pool.getConnection((err, connection) => {
+      if (err) {
+        reject(err)
+      } else {
+        connection.query(sql, values, (err, rows) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve(rows)
+          }
+          connection.release()
+        })
+      }
+    })
+  })
+}
+
+module.exports = query
